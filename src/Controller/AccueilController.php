e96645656e3a5a7367a81f28731286526a13a508
BastienRoux <bastienrouxgame@gmail.com>
2024-11-09 17:34:58 +0100

    update AccueilController.php

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController
 {
     /**
-     * @Route("/accueil", name="accueil")
+     * @Route("/accueil",name="accueil")
      */
     public function index(): Response
     {
